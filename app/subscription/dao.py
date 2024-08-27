3dfe8d2278a109477a3dd4d8659d1fad90727934
Alexey Lisanskiy <alekseylisanskiy@gmail.com>
2024-08-27 19:03:55 +0300

    some optimization

--- app/subscription/dao.py
+++ app/subscription/dao.py
@@ -7,13 +7,14 @@ from app.subscription.models import Subscriptions
 from app.users.models import Users
 from sqlalchemy import select, func, Interval, cast, String, text, Date, column, VARCHAR, Select, Insert, and_
 from sqlalchemy.dialects.postgresql import INTERVAL, JSONB
+from app.users.dao import cast_birthday_to_current_year
 
 
 class SubscriptionsDAO(BaseDAO):
     model = Subscriptions
 
     @classmethod
-    async def test_subs(cls):
+    async def test_subs_v1(cls):
         '''
         SELECT *
 FROM subscriptions
@@ -38,13 +39,36 @@ lpad(CAST(EXTRACT(day FROM users.birthday) AS VARCHAR), 2, '0')) AS DATE) - CURR
             result = await session.execute(query)
             return result.mappings().all()
 
+    @classmethod
+    async def test_subs_v2(cls):
+        '''
+        SELECT *
+FROM subscriptions
+JOIN users ON users.user_id = subscriptions.user_sub_id
+WHERE subscriptions.notify_before_days::jsonb @> to_jsonb(CAST(concat(
+CAST(EXTRACT(year FROM CURRENT_DATE) AS VARCHAR),
+lpad(CAST(EXTRACT(month FROM users.birthday) AS VARCHAR), 2, '0'),
+lpad(CAST(EXTRACT(day FROM users.birthday) AS VARCHAR), 2, '0')) AS DATE) - CURRENT_DATE)
+        '''
+        async with async_session_maker() as session:
+            birthday_this_year = await cast_birthday_to_current_year(Users.birthday)
+            query = select(
+                Subscriptions.__table__.columns,
+                Users.__table__.columns
+            ).join(Subscriptions, Users.user_id == Subscriptions.user_sub_id).where(
+                Subscriptions.notify_before_days.contains(cast(func.to_jsonb(
+                    birthday_this_year - func.current_date()), JSONB))
+            )
+            result = await session.execute(query)
+            return result.mappings().all()
 
 
-# async def main():
-#     task = asyncio.create_task(SubscriptionsDAO.test_subs())
-#     await asyncio.gather(task)
-#     # coro1 = UsersDAO.test()
-#     # await coro1
-#
-#
-# asyncio.get_event_loop().run_until_complete(main())
+
+async def main():
+    task = asyncio.create_task(SubscriptionsDAO.test_subs_v2())
+    await asyncio.gather(task)
+    # coro1 = UsersDAO.test()
+    # await coro1
+
+
+asyncio.get_event_loop().run_until_complete(main())
