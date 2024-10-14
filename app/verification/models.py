1763d67bc99a6c43f2c59ce9464daaf1c1348b9a
Alexey Lisanskiy <alekseylisanskiy@gmail.com>
2024-10-15 02:47:09 +0300

    deleted verification folder

--- app/verification/models.py
+++
@@ -1,9 +0,0 @@
-from app.database import Base
-from sqlalchemy.orm import Mapped
-
-
-class Verification(Base):
-    __tablename__ = 'verification'
-
-    email: Mapped[str]
-    verified: Mapped[bool]
